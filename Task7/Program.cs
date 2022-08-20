267941c5d4e8f95fcdcf1faff8759b1e05e008bf
Alexander <alexandrkudryash@mail.ru>
2022-08-20 23:01:47 +0300

    added solution Task 7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,2 +1,13 @@
 ﻿// ДЗ к Семинару-2 от 08.08.2022 
 // Задача-7. Определить количество цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int i = 0;
+while (n >0)
+{
+   n = n /=10;
+   i++;
+}
+Console.WriteLine(i);
