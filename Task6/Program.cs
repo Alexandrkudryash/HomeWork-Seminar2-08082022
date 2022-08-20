ef5e44ef01a51a10c7a2a285cd68cdd4b8c664ed
Alexander <alexandrkudryash@mail.ru>
2022-08-20 08:53:45 +0300

    added Task-6 solution

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,32 @@
 ﻿// ДЗ к Семинару-2 от 08.08.2022 
 // Задача-6. Найти сумму чисел от 1 до А.
+
+Console.Clear();
+
+Console.WriteLine("введите размер массива");
+int size = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[size];
+array[0] = 1;
+Console.WriteLine();
+Console.WriteLine("получается такой массив:");
+Console.Write(array[0] +", ");
+int j = 1;
+while (j < size)
+{
+    array[j] = array[j-1] + 1;
+    Console.Write(array[j] +", ");
+    j++;
+}
+Console.WriteLine();
+Console.WriteLine("Сумма чисел от 1 до введенного числа:");
+
+int i = 1;
+int sum = array [0];
+while (i < size)
+ {
+    array[i] = array[i-1]+1;
+    sum = sum+array[i];
+    i++;
+}
+Console.WriteLine(sum);
