90144332f21bddf1e0d8948e15032afc62a81a0c
Alexander <alexandrkudryash@mail.ru>
2022-08-21 00:15:25 +0300

    added Task 8  solution

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -4,11 +4,33 @@
 Console.Clear();
 Console.WriteLine("Введите число");
 int n = int.Parse(Console.ReadLine() ?? "0");
+int temp = n;
 int i = 0;
-while (n >0)
+while (n > 0)
 {
    n = n /=10;
    i++;
 }
-Console.WriteLine(i);
+// Console.WriteLine(i);
 // выше код - мы узнали сколько цифр в числе
+
+Console.WriteLine("Запишем наше число в массив по каждой цифре");
+string z = temp.ToString();
+int[] array = new int[i];
+int j = 0;
+while (j < i)
+{
+    array[j] = int.Parse(z[j].ToString());
+    Console.Write(array[j] +", ");
+    j++;
+}
+Console.WriteLine();
+Console.WriteLine("Просуммируем значения массива :");
+int k = 0;
+int sum =0;
+while (k < i)
+{
+   sum = sum +array[k];
+   k++;
+}
+Console.WriteLine(sum);
