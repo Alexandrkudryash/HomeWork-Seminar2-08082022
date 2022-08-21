a66b77630988a64702469303b9e0d60c738f74bf
Alexander <alexandrkudryash@mail.ru>
2022-08-21 16:37:17 +0300

    added Task9 Solution

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -1,2 +1,40 @@
 ﻿// ДЗ к Семинару-2 от 08.08.2022 
 // Задача-9. Написать программу вычисления произведения чисел от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("введите размер массива");
+int size = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[size];
+array[0] = 1;
+Console.WriteLine();
+Console.WriteLine("получается такой массив:");
+Console.Write(array[0] +", ");
+int i = 1;
+while (i < size)
+{
+    array[i] = array[i-1] + 1;
+    Console.Write(array[i] +", ");
+    i++;
+}
+Console.WriteLine();
+Console.WriteLine("Произведение чисел от 1 до введенного числа:");
+
+int Factorial (int size)
+{
+    if (size == 1) return 1;
+    else return size * Factorial(size-1);
+}
+Console.WriteLine(Factorial(size));
+
+/*
+int j = 1;
+int z = 1;
+while (j < size)
+ {
+    int x = x * array[j];
+    Console.Write(x +", ");
+    j++;
+ }*/
+Console.WriteLine();
